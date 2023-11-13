3064a55124af089a1bcf23e2c1c76b999b9b16bd
aulicna <106449030+andreaulicna@users.noreply.github.com>
2023-11-13 21:20:53 +0100

    Created first threads. Perfom_routine is just an arbitrary function printing something to show that the thread run. Still need to check if they are joined and ended successfuly.

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: aulicna <aulicna@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 13:03:10 by aulicna           #+#    #+#             */
-/*   Updated: 2023/11/13 08:42:37 by aulicna          ###   ########.fr       */
+/*   Updated: 2023/11/13 21:19:26 by aulicna          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,13 @@ int	init_party(t_party *party, t_input *input)
 {
 	int	i;
 
+	party->input = input;
 	party->philos = (t_philo *) malloc(sizeof(t_philo) * input->num_philos);
 	if (!party->philos)
 		return (error('A'));
 	i = 0;
 	while (i < input->num_philos)
 	{
-		party->philos[i].thread = 
 		party->philos[i].id = i;
 		party->philos[i].meals_count = 0;
 		party->philos[i].last_meal = 0;
@@ -59,6 +59,34 @@ void	free_party(t_party *party)
 	free(party->philos);
 }
 
+void	*perform_routine(void *party)
+{
+	(void) party;
+	printf("%-5ld %-1d %s\n", get_time(), 2, "runs.");
+	return (NULL);
+}
+
+void handle_threads(t_party *party)
+{
+	int	i;
+
+	i = 0;
+	party->meet = get_time();
+	while(i < party->input->num_philos)
+	{
+		party->philos[i].last_meal = get_time();
+		pthread_create(&party->philos[i].thread, NULL, &perform_routine,
+				&party->philos[i]);
+		i++;
+	}
+	i = 0;
+	while (i < party->input->num_philos)
+	{
+		pthread_join(party->philos[i].thread, NULL);
+		i++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	t_input	input;
@@ -71,9 +99,10 @@ int	main(int argc, char **argv)
 		if (input.num_philos == 1)
 			only_one(&input);
 		init_party(&party, &input);
+		handle_threads(&party);
+		free_party(&party);
 	}
 	else
 		printf("Input error: Wrong number of arguments received.\n");
-	free_party(&party);
 	return (0);
 }
